2640779a531143e53d504c1de35298a5b2725835
prashanth2028 <prashanth@sparkouttech.com>
2024-10-04 16:19:01 +0530

    connect database and port

---
+++ index.js
@@ -0,0 +1,12 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import connectDB from './config/db.js';
+
+dotenv.config();
+connectDB();
+
+const app = express();
+app.use(express.json());
+
+const PORT = process.env.PORT;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
