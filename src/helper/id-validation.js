b45665c4bb4cd0ebc79dc895fedca43037e39813
prashanth2028 <prashanth@sparkouttech.com>
2024-10-08 18:10:34 +0530

    removed logs

--- src/helper/id-validation.js
+++ src/helper/id-validation.js
@@ -3,13 +3,9 @@ import mongoose from "mongoose";
 export const idValidator = (id) => {
   return new Promise((resolve, reject) => {
     
-    if (mongoose.Types.ObjectId.isValid(id)) {
-        console.log(11);
-        
+    if (mongoose.Types.ObjectId.isValid(id)) {        
       resolve(true);
-    } else {
-        console.log(222);
-        
+    } else {        
       reject(false);
     }
   });
